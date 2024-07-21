cb16e464043693ba56b73bf2c961a1e33a302a05
Sam <samueldavidanderson2001@gmail.com>
2024-07-21 15:42:51 -0700

    added moving enemy

--- Project_Alpha/main.cpp
+++ Project_Alpha/main.cpp
@@ -8,7 +8,42 @@ int main()
     
     //fps limit
     window.setFramerateLimit(60);
+    //create snowball
+    sf::Texture ballTexture;
+    sf::Sprite ballSprite;
+    if (ballTexture.loadFromFile("Assets/Enemy/Textures/snowball.png"))
+    {
+        std::cout << "Snowball Loaded" << std::endl;
+        ballSprite.setTexture(ballTexture);
+    }
 
+    sf::CircleShape ball(50.f);
+    ball.setFillColor(sf::Color::Red);
+    ball.setOrigin(25, 25);
+    ball.setPosition(1000, 400);
+
+    //ball logic
+    float xstep = 5;
+    float ystep = 5;
+    float enemymove = 2.5;
+    // create enemy player
+    sf::Texture enemyTexture;
+    sf::Sprite enemySprite;
+    if (enemyTexture.loadFromFile("Assets/Enemy/Textures/elementals.png"))
+    {
+        std::cout << "Enemy model loaded" << std::endl;
+        enemySprite.setTexture(enemyTexture);
+        int x = 0;
+        int y = 0;
+        enemySprite.setTextureRect(sf::IntRect(x * 0, y * 0, 72, 88));
+        enemySprite.setOrigin(36, 44);
+        enemySprite.setPosition(1476, 120);
+        enemySprite.setScale(4, 4);
+    }
+    else
+    {
+        std::cout << "Enemy Image not loaded" << std::endl;
+    }
     //create player
     sf::Texture pTexture; //texture for the player
     sf::Sprite  pSprite; //sprite for the player 
@@ -20,6 +55,7 @@ int main()
         int xIndex = 1;
         int yIndex = 0;
         pSprite.setTextureRect(sf::IntRect(xIndex * 32, yIndex * 32, 32, 32));
+        pSprite.setOrigin(16, 16);
         pSprite.setScale(4, 4);
         pSprite.setPosition(778, 390);
 
@@ -85,6 +121,22 @@ int main()
             int yIndex = 3;
             pSprite.setTextureRect(sf::IntRect(xIndex * 32, yIndex * 32, 32, 32));
         }
+        //moving enemy
+        enemySprite.move(0, enemymove);
+        if (enemySprite.getPosition().y == 120 || enemySprite.getPosition().y == 630)
+        {
+            enemymove = -enemymove;
+        }
+        //moving the ball
+        ball.move(xstep, ystep);
+        if (ball.getPosition().x == 1550 || ball.getPosition().x == 25)
+        {
+            xstep = -xstep;
+        }
+        if (ball.getPosition().y == 710 || ball.getPosition().y == 25)
+        {
+            ystep = -ystep;
+        }
         //clear black color 
         window.clear(sf::Color::Black);
 
@@ -93,6 +145,10 @@ int main()
         // window.draw(...);
         window.draw(backS);
         window.draw(pSprite);
+        window.draw(ball);
+        window.draw(ballSprite);
+        window.draw(enemySprite);
+        
         // end the current frame
         window.display();
 
